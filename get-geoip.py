c731f294eafb5e555bff41d06f1724831fb0ed17
eye9poob <mr.wurst@inbox.ru>
2014-06-05 09:31:23 +0200

    Create get-geoip.py

---
+++ get-geoip.py
@@ -0,0 +1,9 @@
+def main():
+	def mapi():
+		print("Getting GEO IP")
+		ip = urllib.urlopen('http://www.telize.com/geoip').read()
+		print(ip)
+	mapi()
+main()
+
+#Telize.com is awesome go there they have more API's
